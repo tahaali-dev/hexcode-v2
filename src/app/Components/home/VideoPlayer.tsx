4a68f381b4665ab001880bb1cc360808687700b7
tahaali-dev <tahaali.css@gmail.com>
2025-08-27 21:11:37 +0530

    video comp remove unwanted props

--- src/app/Components/home/VideoPlayer.tsx
+++ src/app/Components/home/VideoPlayer.tsx
@@ -1,96 +1,46 @@
 "use client"
 import styled from "@emotion/styled";
-import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { DashedContainer } from "../Containers";
 gsap.registerPlugin(ScrollTrigger);
 
 const VideoPlayer: React.FC = () => {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (wrapperRef.current) {
-      gsap.fromTo(
-        wrapperRef.current,
-        {
-          y: 150,
-          opacity: 0,
-        },
-        {
-          y: 0,
-          opacity: 1,
-          duration: 1.1,
-          ease: "power3.out",
-        }
-      );
-    }
-  }, []);
 
   return (
-    <DashedContainer leftBottom rightBottom>
-      <Wrapper ref={wrapperRef}>
-        <video
-          src="https://screenrepo-production-bucket.s3.ap-south-1.amazonaws.com/videos/Volt+Money+Music+V2+(1)_1.mp4"
-          autoPlay
-          loop
-          muted
-          playsInline
-        />
-        <BottomFade />
-      </Wrapper>
-    </DashedContainer >
+    <Wrapper>
+      <video
+        src="https://screenrepo-production-bucket.s3.ap-south-1.amazonaws.com/videos/Volt+Money+Music+V2+(1)_1.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+    </Wrapper>
   );
 };
 
 export default VideoPlayer;
 
 const Wrapper = styled.div`
-  position: relative;
-  z-index: 0;
   margin-top:64px;
-  border:1px solid red;
-  padding:8px;
-
-  border-radius : 22px;
-  overflow:hidden;
-  background-color: 
-    color-mix(in oklab, #fff 20%, transparent);
-  border-color: #fff3;
+  display:flex;
+  justify-content:center;
 
   video{
-    width: 100%;
-    height: 100%;
-    z-index: 0;
     border-radius : 22px;
-    display: block;
+    width: 1240px;
+    background-color: 
+    color-mix(in oklab, #fff 20%, transparent);
+   border: 1px solid #fff3;
+   padding:8px;
   }
 
     @media (max-width: 768px) {
       margin-top:32px;
-      border-radius : 8px;
-        padding:4px;
+      padding:4px;
 
         video{
-    border-radius : 8px;
-  }
+         border-radius : 8px;
+        }
     }
-`;
-
-// Gradient overlay for bottom fade
-const BottomFade = styled.div`
-  pointer-events: none;
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 32%; /* adjust as needed */
-  border-radius: 0 0 22px 22px;
-  background: linear-gradient(
-    to bottom,
-    rgba(255,255,255,0) 0%,
-    rgba(0,0,0,0.12) 60%,
-    rgba(0,0,0,0.32) 100%
-  );
-  z-index: 2;
 `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import EmptyContainer from "./Components/Containers"
 import AboutUs from "./Components/home/AboutUs"
 import Benefits from "./Components/home/Benefits"
-import CompaniesWeWork from "./Components/home/CompaniesWeWork"
 import Faqs from "./Components/home/Faqs"
-import MobileComp from "./Components/home/MobileComp"
 import OurCapabilities from "./Components/home/OurCapabilities"
 import PrimeFold from "./Components/home/PrimeFold"
 import Projects from "./Components/home/Projects"
 import TestimonialSlider from "./Components/home/TestimonialSlider"
-import VideoPlayer from "./Components/home/VideoPlayer"
 
 const page = () => {
-
 
   return (
     <>
